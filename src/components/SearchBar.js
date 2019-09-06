fc06206fbb5d1614ee7a127e8bbfa213c345041b
Natdenice <clad1987@hotmail.com>
2019-09-06 16:51:04 +0200

    ui container added in App.js and basic search bar created with a label, a form and a input with a placeholder

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,16 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Search Your Video</label>
+            <input type="text" placeholder="How to learn React.js ?" />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
