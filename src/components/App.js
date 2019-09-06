157b956192f48934bb1c578ddd8bf25d20559509
Natdenice <clad1987@hotmail.com>
2019-09-06 16:31:50 +0200

    SearchBar class component added and imported in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
   }
 }
 
---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return <div>SearchBar</div>;
+  }
+}
+
+export default SearchBar;
