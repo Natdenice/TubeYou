85dbcd1af72c46fe83f48e8b1308e94eca8de9e8
Natdenice <clad1987@hotmail.com>
2019-09-06 18:35:30 +0200

    handleOnChange method created in SearchBar component, value property and onChange eventListener added in input tag in order to call handleOnChange method as soon the user types a term inside the SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,13 +2,22 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state={
+    term: ""
+  }
+
+  handleOnChange = (e) => {
+    this.setState({term: e.target.value});
+    console.log(this.state.term);
+  }
+
   render() {
     return (
       <div className="search-bar ui segment">
         <form className="ui form">
           <div className="field">
             <label>Search Your Video</label>
-            <input type="text" placeholder="How to learn React.js ?" />
+            <input value={this.state.term} onChange={this.handleOnChange} type="text" placeholder="How to learn React.js ?" />
           </div>
         </form>
       </div>
