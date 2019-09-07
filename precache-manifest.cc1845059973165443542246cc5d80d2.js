7f5e42b79ed96f2a15d8f6451a464f6b6a35f422
Natdenice <clad1987@hotmail.com>
2019-09-07 16:20:24 +0200

    Updates

---
+++ precache-manifest.cc1845059973165443542246cc5d80d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6458588ebd5f8fce8ac97234271ea4f9",
+    "url": "/TubeYou/index.html"
+  },
+  {
+    "revision": "0489c16f90993893a12d",
+    "url": "/TubeYou/static/css/main.84cc5cc7.chunk.css"
+  },
+  {
+    "revision": "3ca9472539a7a3966b6a",
+    "url": "/TubeYou/static/js/2.4c2b1ef2.chunk.js"
+  },
+  {
+    "revision": "0489c16f90993893a12d",
+    "url": "/TubeYou/static/js/main.4be94070.chunk.js"
+  },
+  {
+    "revision": "0fbbc17bbc5fb956f3a8",
+    "url": "/TubeYou/static/js/runtime~main.21f23277.js"
+  }
+]);
