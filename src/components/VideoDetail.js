bf6e7e8a7175e86bc78dcf648f06619a51205dba
Natdenice <clad1987@hotmail.com>
2019-09-07 15:12:19 +0200

    ui segment added into VideoDetail component in order to add more readability about title and description at the bottom of the videoplayer

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -6,10 +6,14 @@ const VideoDetail = ({ video }) => {
   }
   const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}?autoplay=1&enable_js=1`;
   return (
-    <div className="ui embed">
-      <iframe src={videoSrc} allow="autoplay" allowFullScreen />
-      <h4>{video.snippet.title}</h4>
-      <p>{video.snippet.description}</p>
+    <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} allow="autoplay" allowFullScreen />
+      </div>
+      <div className="ui segment">
+        <h4>{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
     </div>
   );
 };
