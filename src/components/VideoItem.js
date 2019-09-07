af900daffd88a9d44ca22c991458a06c1e340a64
Natdenice <clad1987@hotmail.com>
2019-09-07 14:01:42 +0200

    VideoList.css deleted (useless) and VideoItem.css created and stylized

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -7,8 +7,8 @@ const VideoItem = ({ video }) => {
     <div className="video-item item">
       <img
         className="ui image"
-        src={video.snippet.thumbnails.medium.url}
         alt={video.snippet.title}
+        src={video.snippet.thumbnails.medium.url}
       />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
